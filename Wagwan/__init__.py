46610414898993714a395c81533c6cb404cda201
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-10 23:20:49 +0200

    nuances

--- Wagwan/__init__.py
+++ Wagwan/__init__.py
@@ -1,9 +1,11 @@
 """
 The flask application package.
 """
-
+import os
 from flask import Flask
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
-app.secret_key = "493wtgZewc9un"
-import Wagwan.views
+app.secret_key = os.urandom(24)
+WWW_ROOT = "Wagwan"
+PUBLIC_DIR = "static"
+from Wagwan import api, views
